7b7bed2db36235a8bd6b982ddfa9e2081b881121
miro-sw <miro.ashutosh@gmail.com>
2026-02-05 16:01:02 +0530

    organization url removed

--- institute/urls.py
+++ institute/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('delete-user/<int:user_id>/', views.delete_user, name='delete_user'),
     path('admission-form/', views.admission_form, name='admission_form'),
     path('search-admission/', views.search_admission, name='search_admission'),
-    path('register-organization/', views.register_organization, name='register_organization'),
     # Account Management URLs
     path('account-section/', views.account_section, name='account_section'),
     path('student-account/<int:admission_id>/', views.student_account, name='student_account'),
--- mother_institute/settings.py
+++ mother_institute/settings.py
@@ -101,6 +101,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Custom User Model
